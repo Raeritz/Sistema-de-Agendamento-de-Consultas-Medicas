45dbca161ea45c45515680842bb1db6a689a66d2
Manoel <120431500+Raeritz@users.noreply.github.com>
2025-02-23 22:10:41 +0000

    Create Paciente.java

---
+++ Paciente.java
@@ -0,0 +1,7 @@
+public class Paciente extends Pessoa{
+    private List<String> consulta_marcada; //?????? acho que ta legal isso nao
+    
+    public Paciente(){
+        this.consulta_marcada = consulta_marcada;
+    }
+}
